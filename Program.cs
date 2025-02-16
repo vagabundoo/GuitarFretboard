e8944761df7c3d437df83020660974724c6b2893
vagabundoo <evillamorm@gmail.com>
2025-02-16 18:19:19 +0100

    Added support for asking for notes accross multiple strings.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Spectre.Console;
 using GuitarFretboard;
 
+Console.Clear();
+
 /*
 string[] lines = File.ReadAllLines("input/guitarFretboardEmpty.txt");
 
@@ -12,33 +14,57 @@ var fretboard = new Fretboard();
 fretboard.PrintFretboard();
 
 // GuitarString functionality
-AnsiConsole.MarkupLine("[blue]Hello[/] [red] world![/]");
+AnsiConsole.MarkupLine("[blue]Welcome to GuitarNote[/] [red] Trainer![/]");
 var estring = new GuitarString("E");
 
-System.Console.WriteLine(estring.NoteNames[3]);
-System.Console.WriteLine($"{String.Join(" ",estring.NoteNames)}");
+//System.Console.WriteLine(estring.NoteNames[3]);
+//System.Console.WriteLine($"{String.Join(" ",estring.NoteNames)}");
+
+var tuning = new string[] {"E", "A", "D", "G", "B", "e"};
+List<List<string>> guitarStrings = new List<List<string>>();
+/*
+for (int i = 0; i < guitarStrings.Count(); i++)
+{
+    guitarStrings[i] = new GuitarString(tuning[i]).NoteNames;
+}*/
+
+Dictionary<string, string[]> standardStrings = new Dictionary<string, string[]>
+{
+    {"e", ["E", "F", "F#", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb"]},
+    {"B", ["B", "C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb"]},
+    {"G", ["G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "F#"]},
+    {"D", ["D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B", "C", "Db"]},
+    {"A", ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab"]},
+    {"E", ["E", "F", "F#", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb"]}
+};
+
+//var rand = new Random();
+//var randString = ;
+//System.Console.WriteLine(randString);
 
 string? givenAnswer = "";
 do
 {
-    var randomFret = new Random();
-    var chosenFret = randomFret.Next(0,12);
+    var random = new Random();
+    var randomFret = random.Next(0,12);
+    string randomGuitarString = standardStrings.ElementAt(random.Next(0, standardStrings.Count())).Key;
+    var randomNotes = standardStrings[randomGuitarString];
     
-    var requiredNote = estring.NoteNames[chosenFret].ToLower();
-    System.Console.WriteLine(requiredNote);
+    var requiredNote = randomNotes[randomFret];
+    System.Console.WriteLine($"Hint: {requiredNote}");
 
-    AnsiConsole.MarkupLine($"Give the notename at fret {chosenFret}:");
+    AnsiConsole.MarkupLine("[blue] Give the note name [/]");
+    AnsiConsole.MarkupLine($"String {randomGuitarString}, fret {randomFret}:");
     
     givenAnswer = Convert.ToString(Console.ReadLine());
-    givenAnswer = givenAnswer.ToLower();
 
-    if (givenAnswer == requiredNote) 
+    if (givenAnswer.ToLower() == requiredNote.ToLower()) 
     {
         AnsiConsole.MarkupLine("[green] Correct![/]");
     } 
     else 
     {
-        AnsiConsole.MarkupLine("[red] Wrong[/]. Try again.");
+        AnsiConsole.MarkupLine($"[red] Wrong[/]. The correct answer was [blue]{requiredNote}[/].");
     }
     
 
